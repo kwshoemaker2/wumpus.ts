2ee1ee20280809b6869ff90fee430cc226d62c58
kyleshoe <kyleshoe2@gmail.com>
2020-10-25 14:34:15 -0700

    Break out the player actions to their own file

--- src/game.ts
+++ src/game.ts
@@ -1,82 +1,8 @@
-
 import { WumpusCave } from './wumpusCave'
 import { WumpusRoom } from './wumpusRoom'
 import { WumpusDisplay } from './wumpusDisplay'
 import { WumpusCommand, WumpusAction } from './wumpusAction';
-
-/**
- * Abstraction for an action a player can perform.
- */
-interface PlayerAction {
-
-    /**
-     * Perform the action.
-     * 
-     * Returns true if the game is still running.
-     */
-    perform(cave: WumpusCave, display: WumpusDisplay): boolean;
-}
-
-/**
- * Handles quitting the game.
- */
-class QuitGame implements PlayerAction {
-    perform(cave: WumpusCave, display: WumpusDisplay): boolean {
-        // FUTURE Prompt the user if they want to quit.
-        cave;
-        display;
-        return false;
-    }
-}
-
-/**
- * Handles moving the player.
- */
-class MovePlayer implements PlayerAction {
-
-    private roomNumber: number;
-
-    constructor(roomNumber: number) {
-        this.roomNumber = roomNumber;
-    }
-
-    perform(cave: WumpusCave, display: WumpusDisplay): boolean {
-        return this.movePlayer(cave, display);
-    }
-
-    private movePlayer(cave: WumpusCave,
-                       display: WumpusDisplay): boolean
-    {
-        let playerSurvived: boolean = true;
-
-        if(cave.adjacentRoom(this.roomNumber)) {
-            cave.move(this.roomNumber);
-            const currentRoom = cave.getCurrentRoom();
-            if(currentRoom.hasPit()) {
-                display.showPlayerFellInPit();
-                playerSurvived = false;
-            }
-        } else {
-            display.showPlayerHitWall();
-        }
-
-        return playerSurvived
-    }
-}
-
-function createAction(action: WumpusAction): PlayerAction {
-    if(action.command === WumpusCommand.Move) {
-        const roomNumber = action.args[0];
-        return new MovePlayer(roomNumber);
-    }
-    else if(action.command === WumpusCommand.Quit) {
-        return new QuitGame();
-    }
-    else
-    {
-        return undefined;
-    }
-}
+import { PlayerAction, PlayerActionFactory, PlayerActionFactoryImp } from './playerAction';
 
 /**
  * Hunt the Wumpus game.
@@ -84,11 +10,13 @@ function createAction(action: WumpusAction): PlayerAction {
 export class Game {
     private cave: WumpusCave;
     private display: WumpusDisplay;
+    private playerActionFactory: PlayerActionFactoryImp;
 
     public constructor(cave: WumpusCave,
                        display: WumpusDisplay) {
         this.cave = cave;
         this.display = display;
+        this.playerActionFactory = new PlayerActionFactoryImp();
     }
 
     /**
@@ -104,12 +32,23 @@ export class Game {
     private async gameLoop(): Promise<void> {
         let running: boolean = true;
         while(running) {
-            let currentRoom: WumpusRoom = this.cave.getCurrentRoom();
-            this.display.showRoomEntry(currentRoom);
-
-            const nextAction = await this.display.getUserAction();
-            const userAction = createAction(nextAction);
-            running = userAction.perform(this.cave, this.display);
+            this.displayCurrentRoom();
+            const nextAction = await this.getNextAction();
+            running = this.doAction(nextAction);
         }
+    }
+
+    private displayCurrentRoom(): void {
+        let currentRoom: WumpusRoom = this.cave.getCurrentRoom();
+        this.display.showRoomEntry(currentRoom);
+    }
+
+    private async getNextAction(): Promise<WumpusAction> {
+        return await this.display.getUserAction();
+    }
+
+    private doAction(action: WumpusAction): boolean {
+        const userAction = this.playerActionFactory.createPlayerAction(action);
+        return userAction.perform(this.cave, this.display);
     }
 }
---
+++ src/playerAction.ts
@@ -0,0 +1,91 @@
+import { WumpusCave } from './wumpusCave'
+import { WumpusRoom } from './wumpusRoom'
+import { WumpusDisplay } from './wumpusDisplay'
+import { WumpusCommand, WumpusAction } from './wumpusAction';
+
+/**
+ * Abstraction for an action a player can perform.
+ */
+export interface PlayerAction {
+
+    /**
+     * Perform the action.
+     * 
+     * Returns true if the game is still running.
+     */
+    perform(cave: WumpusCave, display: WumpusDisplay): boolean;
+}
+
+/**
+ * Handles quitting the game.
+ */
+class QuitGame implements PlayerAction {
+    perform(cave: WumpusCave, display: WumpusDisplay): boolean {
+        // FUTURE Prompt the user if they want to quit.
+        cave;
+        display;
+        return false;
+    }
+}
+
+/**
+ * Handles moving the player.
+ */
+class MovePlayer implements PlayerAction {
+
+    private roomNumber: number;
+
+    constructor(roomNumber: number) {
+        this.roomNumber = roomNumber;
+    }
+
+    perform(cave: WumpusCave, display: WumpusDisplay): boolean {
+        return this.movePlayer(cave, display);
+    }
+
+    private movePlayer(cave: WumpusCave, display: WumpusDisplay): boolean {
+        let playerSurvived: boolean = true;
+
+        if(cave.adjacentRoom(this.roomNumber)) {
+            cave.move(this.roomNumber);
+            const currentRoom = cave.getCurrentRoom();
+            if(currentRoom.hasPit()) {
+                display.showPlayerFellInPit();
+                playerSurvived = false;
+            }
+        } else {
+            display.showPlayerHitWall();
+        }
+
+        return playerSurvived
+    }
+}
+
+/**
+ * Factory class that makes PlayerAction objects.
+ */
+export interface PlayerActionFactory {
+    /**
+     * Create the PlayerAction from the WumpusAction.
+     * @param action 
+     */
+    createPlayerAction(action: WumpusAction): PlayerAction;
+}
+
+export class PlayerActionFactoryImp implements PlayerActionFactory {
+
+    createPlayerAction(action: WumpusAction): PlayerAction {
+        if(action.command === WumpusCommand.Move) {
+            const roomNumber = action.args[0];
+            return new MovePlayer(roomNumber);
+        }
+        else if(action.command === WumpusCommand.Quit) {
+            return new QuitGame();
+        }
+        else
+        {
+            return undefined;
+        }
+    }
+}
+
