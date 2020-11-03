4c4c56977d2c48ac516857f938af36b7ccfc61e3
kyleshoe2 <kyleshoe2@gmail.com>
2020-11-02 23:04:04 -0800

    Update name to initialEvent

--- src/playerAction.test.ts
+++ src/playerAction.test.ts
@@ -3,19 +3,19 @@ import * as tsSinon from 'ts-sinon'
 import { WumpusCave } from './wumpusCave'
 import { WumpusDisplay } from './wumpusDisplay'
 import { GameEventProcessor } from './playerAction';
-import { GameEvent, GameOverEvent, PlayerIdleEvent, PlayerMovedToRoomEvent } from './gameEvent'
+import { GameEvent, GameOverEvent, PlayerIdleEvent } from './gameEvent'
 
 describe("GameEventProcessor", () => {
     let cave: tsSinon.StubbedInstance<WumpusCave> = null;
     let display: tsSinon.StubbedInstance<WumpusDisplay> = null;
-    let playerMovedToRoomEvent: tsSinon.StubbedInstance<GameEvent> = null;
+    let initialEvent: tsSinon.StubbedInstance<GameEvent> = null;
     let gameEventProcessor: GameEventProcessor = null;
 
     beforeEach(() => {
         cave = tsSinon.stubInterface<WumpusCave>();
         display = tsSinon.stubInterface<WumpusDisplay>();
-        playerMovedToRoomEvent = getGameEventStub();
-        gameEventProcessor = new GameEventProcessor(playerMovedToRoomEvent);
+        initialEvent = getGameEventStub();
+        gameEventProcessor = new GameEventProcessor(initialEvent);
     });
 
     function getGameEventStub(): tsSinon.StubbedInstance<GameEvent> {
@@ -23,7 +23,7 @@ describe("GameEventProcessor", () => {
     }
 
     it("indicates game is running when the next event is that the player is idle", () => {
-        playerMovedToRoomEvent.perform.returns(new PlayerIdleEvent());
+        initialEvent.perform.returns(new PlayerIdleEvent());
 
         const gameRunning = gameEventProcessor.perform(cave, display);
 
@@ -31,7 +31,7 @@ describe("GameEventProcessor", () => {
     });
 
     it("indicates game is not running when the next event is that the game is over", () => {
-        playerMovedToRoomEvent.perform.returns(new GameOverEvent());
+        initialEvent.perform.returns(new GameOverEvent());
 
         const gameRunning = gameEventProcessor.perform(cave, display);
 
@@ -43,13 +43,13 @@ describe("GameEventProcessor", () => {
         const gameEvent2 = getGameEventStub();
         const gameEvent3 = new PlayerIdleEvent();
 
-        playerMovedToRoomEvent.perform.returns(gameEvent1);
+        initialEvent.perform.returns(gameEvent1);
         gameEvent1.perform.returns(gameEvent2);
         gameEvent2.perform.returns(gameEvent3);
 
         gameEventProcessor.perform(cave, display);
 
-        expect(playerMovedToRoomEvent.perform.calledOnce).equals(true);
+        expect(initialEvent.perform.calledOnce).equals(true);
         expect(gameEvent1.perform.calledOnce).equals(true);
         expect(gameEvent2.perform.calledOnce).equals(true);
     });
